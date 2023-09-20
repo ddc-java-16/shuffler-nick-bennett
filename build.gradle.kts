d0cec22bbc67dfa602a1998a17b375280ebb4937
nick-bennett <nbennett10@cnm.edu>
2023-09-20 15:35:25 -0600

    Take data to shuffle from command line.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,14 @@ application {
     mainClass.set(project.property("mainClass") as String)
 }
 
+/*
+tasks.run {
+    with (this as JavaExec) {
+        standardInput = System.`in`
+    }
+}
+*/
+
 repositories {
     mavenCentral()
 }
--- src/main/java/edu/cnm/deepdive/shuffle/Main.java
+++ src/main/java/edu/cnm/deepdive/shuffle/Main.java
@@ -1,14 +1,21 @@
 package edu.cnm.deepdive.shuffle;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
 
   public static void main(String[] args) {
-    int[] data = new int[100];
-    for (int i = 0; i < data.length; i++) {
-      data[i] = 100 + i;
+    int[] data = new int[args.length];
+    int i = 0;
+    try {
+      for ( ; i < args.length; i++) {
+        data[i] = Integer.parseInt(args[i]);
+      }
+    } catch (NumberFormatException e) {
+      System.err.printf("Parsing failed! %s%n", e.getMessage());
     }
+    data = Arrays.copyOf(data, i);
     System.out.println(Arrays.toString(data));
     Shuffler shuffler = new Shuffler();
     shuffler.shuffle(data);
