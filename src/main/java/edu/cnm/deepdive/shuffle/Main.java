20731dbae9caabe8d961c58fd2b80bf905fa7a70
nick-bennett <nbennett10@cnm.edu>
2023-09-20 17:08:10 -0600

    Wrote a method to read and parse stdin.

--- src/main/java/edu/cnm/deepdive/shuffle/Main.java
+++ src/main/java/edu/cnm/deepdive/shuffle/Main.java
@@ -1,11 +1,24 @@
 package edu.cnm.deepdive.shuffle;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
+    int[] data = parseCommandLine(args); // TODO Change to parseInputStream()
+    System.out.println(Arrays.toString(data));
+    Shuffler shuffler = new Shuffler();
+    shuffler.shuffle(data);
+    System.out.println(Arrays.toString(data));
+  }
+
+  private static int[] parseCommandLine(String[] args) {
     int[] data = new int[args.length];
     int i = 0;
     try {
@@ -16,10 +29,30 @@ public class Main {
       System.err.printf("Parsing failed! %s%n", e.getMessage());
     }
     data = Arrays.copyOf(data, i);
-    System.out.println(Arrays.toString(data));
-    Shuffler shuffler = new Shuffler();
-    shuffler.shuffle(data);
-    System.out.println(Arrays.toString(data));
+    return data;
   }
 
+  private static int[] parseStandardInput() throws IOException {
+    List<Integer> data = new LinkedList<>();
+    Reader reader = new InputStreamReader(System.in);
+    BufferedReader buffer = new BufferedReader(reader);
+    String input;
+    try {
+      while ((input = buffer.readLine()) != null) {
+        data.add(Integer.parseInt(input.strip()));
+      }
+    } catch (NumberFormatException e) {
+      System.err.printf("Parsing failed! %s%n", e.getMessage());
+    }
+    int[] result = new int[data.size()];
+    int position = 0;
+    for (int value : data) {
+      result[position] = value;
+      position++;
+    }
+    return result;
+//    for (String input = buffer.readLine(); input != null; input = buffer.readLine()) {
+//
+//    }
+  }
 }
